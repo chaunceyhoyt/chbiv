2f74772a81225da8371d92a40cba06a5735e6d3e
chaunceyhoyt <54325688+chaunceyhoyt@users.noreply.github.com>
2019-10-03 13:49:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function onYouTubeIframeAPIReady() {
         height: 378,
         videoId: '9L1hTX-_wYQ',
         playerVars: {
+            autoplay:'1',
             controls: '0',
             loop: '1',
             playlist: 'HlKBymW_2EA,rG1Hhtx7f28'
