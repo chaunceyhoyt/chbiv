268e65d4436677d6ae897060e135d4fe90d4eef9
chaunceyhoyt <54325688+chaunceyhoyt@users.noreply.github.com>
2019-10-03 13:42:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ var player,
 
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('video-placeholder', {
-        width: 649,
-        height: 378,
-        videoId: '9L1hTX-_wYQ'
+        width: 600,
+        height: 400,
+        videoId: 'Xa0Q0J5tOP0',
         playerVars: {
-
-            playlist: 'HlKBymW_2EA,rG1Hhtx7f28'
+            color: 'white',
+            playlist: 'taJ60kskkns,FG0fTKAqZ5g'
         },
         events: {
             onReady: initialize
